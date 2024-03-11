d671be74245f4cab5a7efb2cd562f11d8f5decf4
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2024-03-11 20:42:15 +0530

    Add email & role of the session

--- app/ClientMember/page.jsx
+++ app/ClientMember/page.jsx
@@ -1,8 +1,22 @@
-"use-client";
-const Member = () => {
+"use client";
+
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+
+const Member =  () => {
+  const {data: session} = useSession({
+    required: true,
+
+    onUnauthenticated() {
+      redirect("/api/auth/signin?callbackUrl=/ClientMember");
+    },
+  });
+
   return (
     <div>
-      <h1>Member Server Session</h1>
+      <h1>Member Client Session</h1>
+      <p>{session?.user?.email}</p>
+      <p>{session?.user?.role}</p>
     </div>
   )
 }
