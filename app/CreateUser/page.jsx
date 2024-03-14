e3bd2d07a9d94db3373db05c7e3ed86425d8db62
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2024-03-14 22:38:48 +0530

    Add UserForm

--- app/CreateUser/page.jsx
+++ app/CreateUser/page.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import UserForm from '../components/userForm'
 
 const CreateUser = () => {
   return (
     <div>
-        <h1>Only Admins</h1>
+        <UserForm />
     </div>
   )
 }
